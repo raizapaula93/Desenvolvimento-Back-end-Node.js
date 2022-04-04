f30d2d40741faac61ec62d81329f3d220233bfa4
raizapaula93 <raizapaula93@gmail.com>
2022-04-04 00:16:58 -0300

    @raizapaula93[TiposPrimitivos]

---
+++ IntroEConceitosFund/tiposprimitivos.js
@@ -0,0 +1,19 @@
+//STRING
+//NUMBER -> FLOAT E INT
+//UNDEFINED -> NÃO APONTA PARA NENHUM LOCAL DA MEMÓRIA/ausência, PODE VIR A CRESCER
+//NULL -> É VAZIO DE PROPÓSITO
+//BOOLEAN -> TRUE OR FALSE
+//valor primitivo =/= valor por referência -> valor primit
+//symbol
+
+const nome1= "joão";
+const nome2= 'joão';
+const nome3= `joão`;
+
+console.log(nome1,nome2,nome3);
+
+let value = 'Zagatti'
+let newValue = value
+
+console.log(value,newValue);
+
