4429aadcd205d44aaa36495834973a66178589b1
raizapaula93 <raizapaula93@gmail.com>
2022-04-03 23:46:37 -0300

    @raizapaula93[ConceitosIntroJS]

---
+++ IntroEConceitosFund/constvarlet.js
@@ -0,0 +1,37 @@
+/* Variáveis e Constantes */
+/* console.log("Olá Mundo");
+
+var Variável =0;
+let Variável2 =1;
+const Variável3=2; */
+
+/* let nome= 'João';//se não colocar valor a variável aparece undefined
+
+console.log('Olá, meu nome é', nome);//concatenar string js
+
+nome = 'Maria';
+
+console.log('Olá, meu nome é', nome); */
+//JS se usa PascalCase
+/* Constantes */
+//const name = 'Zé';
+ /* const PrimeiroNumero = 1;
+ const SegundoNumero = 2;
+ const Resultado = PrimeiroNumero + SegundoNumero; */
+ 
+ //console.log(Resultado)
+
+/* Let */
+
+//Var - dá pra redeclarara variável
+//Let - não dá pra redeclarar
+
+const nome = "Rafael";
+const idade = 39;
+const peso = 89;
+const altura = 1.80;
+let imc= peso/(altura*altura);
+
+console.log(nome+" tem " + idade+" anos "  +" pesa "+ peso+" ks " +" e seu imc é: "+ imc)
+
+console.log(`${nome} tem ${idade} anos, pesa ${peso}kg,`);//ecmascript
